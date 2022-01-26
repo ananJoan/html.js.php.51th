a1d48f47ad17b09b2fdcf724aa2ff421dcceea6b
ananJoan <98436245+ananJoan@users.noreply.github.com>
2022-01-26 22:39:40 +0800

    Delete an51th12 directory
    revise version

--- an51th12/lock.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $db=mysqli_connect("localhost","admin","1234","an51th12");
-    mysqli_query($db,"SET NAMES UTF8");
-    $id=$_GET["id"];
-    mysqli_query($db,"UPDATE `ask` SET `locks`='t' WHERE `id` LIKE '$id'");
-    header("location:manage.php")
-?>
