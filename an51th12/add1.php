74712676acf272def7ce0f8f63cec51bc027d406
ananJoan <98436245+ananJoan@users.noreply.github.com>
2022-02-07 11:03:12 +0800

    Update add1.php

--- an51th12/add1.php
+++ an51th12/add1.php
@@ -5,7 +5,7 @@
     $invides="";
     $v=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     for($i=0;$i<=7;$i++){
-        $tp=rand(0,25);
+        $tp=rand(0,52);
         $invides=$invides.$v[$tp];
     }
     if(!empty($_POST)){
